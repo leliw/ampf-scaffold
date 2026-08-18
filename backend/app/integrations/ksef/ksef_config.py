d30d5d47867bcefca131b571d1896eac2de6fe8d
Marcin Leliwa <marcin.leliwa@gmail.com>
2026-08-18 09:00:51 +0200

    Revert "feat(backend): Copy sources from hanza2 project"
    This reverts commit 456c5878092d023f421cc285f55d696f92535f53.

--- backend/app/integrations/ksef/ksef_config.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class KsefConfig(BaseModel):
-    base_url: str
-    token: str
-    nip: str
--- backend/app/shared/mariadb/__init__.py
+++
@@ -1,5 +0,0 @@
-from .mariadb_config import MariaDBConfig
-from .mariadb_connection import MariaDBConnection, MariaDBConnectionProvider
-from .mariadb_engine import MariaDBEngine
-
-__all__ = ["MariaDBConfig", "MariaDBConnection", "MariaDBConnectionProvider", "MariaDBEngine"]
